a444fd241b2df076d2d1803508268ac846dffcba
lesiaturuta <turuta.lesya@gmail.com>
2024-05-19 15:55:28 +0300

    fixed search component

--- src/components/search/Search.component.tsx
+++ src/components/search/Search.component.tsx
@@ -1,40 +1,59 @@
+import React from 'react';
+
 import search from '@/assets/icons/search.svg';
 
 import styles from './search.module.css';
 
-const Search = ({ theme }: { theme: string }) => (
-    <div className={`${styles.main} ${theme === 'light' ? styles.light_theme : styles.dark_theme}`}>
-        <div className={styles.search}>
-            <input className={styles.search_input} type="text" placeholder="Search..." />
-            <button className={styles.btn_icon}>
-                <img className={styles.icon} src={search} alt="" />
-            </button>
-        </div>
-        <div className={styles.change}>
-            <div className={styles.buttons}>
-                <button className={`${styles.btn} ${styles.mr_10}`}>Electronics</button>
-                <button className={`${styles.btn} ${styles.mr_10}`}>Shoes</button>
-                <button className={styles.btn}>Clothes</button>
+const Search = ({ theme }: { theme: string }) => {
+    const nameSelect: string[] = ['Price (High - Low)', 'Price (Low- High)', 'Newest', 'Oldest'];
+
+    const [selectedSort, setSelectedSort] = React.useState<string>(nameSelect[0]);
+    return (
+        <div className={`${styles.main} ${theme === 'light' ? styles.light_theme : styles.dark_theme}`}>
+            <div className={styles.search}>
+                <input className={styles.search_input} type="text" placeholder="Search..." />
+                <button className={styles.btn_icon}>
+                    <img className={styles.icon} src={search} alt="" />
+                </button>
             </div>
-            <div className={styles.sort}>
-                <span className={styles.sort_name}>Sort by:</span>
-                <select className={`${styles.select} ${styles.height_40px}`} name="user_profile_color_1">
-                    <option className={styles.select_item} value="1">
-                        Price (High - Low)
-                    </option>
-                    <option className={styles.select_item} value="2">
-                        Price (Low- High)
-                    </option>
-                    <option className={styles.select_item} value="3">
-                        Newest
-                    </option>
-                    <option className={styles.select_item} value="4">
-                        Oldest
-                    </option>
-                </select>
+            <div className={styles.change}>
+                <div className={styles.buttons}>
+                    <button className={`${styles.btn} ${styles.mr_10}`}>Electronics</button>
+                    <button className={`${styles.btn} ${styles.mr_10}`}>Shoes</button>
+                    <button className={styles.btn}>Clothes</button>
+                </div>
+                <div className={styles.sort}>
+                    <span className={styles.sort_name}>Sort by:</span>
+
+                    <div className={styles.drop_down}>
+                        <button className={styles.select}>{selectedSort}</button>
+                        <div className={styles.drop_down_content}>
+                            {selectedSort !== nameSelect[0] && (
+                                <button className={styles.select_btn} onClick={() => setSelectedSort(nameSelect[0])}>
+                                    Price (High - Low)
+                                </button>
+                            )}
+                            {selectedSort !== nameSelect[1] && (
+                                <button className={styles.select_btn} onClick={() => setSelectedSort(nameSelect[1])}>
+                                    Price (Low- High)
+                                </button>
+                            )}
+                            {selectedSort !== nameSelect[2] && (
+                                <button className={styles.select_btn} onClick={() => setSelectedSort(nameSelect[2])}>
+                                    Newest
+                                </button>
+                            )}
+                            {selectedSort !== nameSelect[3] && (
+                                <button className={styles.select_btn} onClick={() => setSelectedSort(nameSelect[3])}>
+                                    Oldest
+                                </button>
+                            )}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Search;
